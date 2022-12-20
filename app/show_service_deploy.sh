ca7e98d8b2a94a93d93118c3229260dd19f02c73
Momo <76620405+immxmmi@users.noreply.github.com>
2022-12-20 12:36:04 +0100

    update

--- app/show_service_deploy.sh
+++ app/show_service_deploy.sh
@@ -1,6 +1,16 @@
-echo "service"
+echo "| - - - database - - - |"
+echo "| - - - statefulset - - - |"
+kubectl get statefulset --namespace cldinf-app
+echo "| - - - pvc - - - |"
+kubectl get pvc --namespace cldinf-app
+echo "| - - - pv - - - |"
+kubectl get pv --namespace cldinf-app
+echo "| - - - service - - - |"
 kubectl get service --namespace cldinf-app
-echo "deployments"
+echo "| - - - deployments - - - |"
 kubectl get deployments --namespace cldinf-app
-echo "pods"
+echo "| - - - pods - - - |"
 kubectl get pods --namespace cldinf-app
+echo "| - - - ingress - - - |"
+kubectl get ingress -n cldinf-app
+
